583e8e61bebad557d6283949faf708118806a299
Vimlesh Singh <Vimlesh.Singh@evolve-analytics.com>
2019-02-12 09:40:15 +0000

    changed style for filter

--- src/components/EntityTable.jsx
+++ src/components/EntityTable.jsx
@@ -6,26 +6,32 @@ export class EntityTable extends React.Component {
     // TODO: render the entity table content here using React table
 
     const data = this.props.data;
+    const columns = [
+      {
+        Header: 'Entities',
+        columns: [
+          {
+            Header: 'Company Name',
+            accessor: 'name'
+          },
+          {
+            Header: 'Industry Type',
+            accessor: 'industry'
+          }
+        ]
+      }
+    ];
     return (
       <div>
         <ReactTable
           data={data}
-          columns={[
-            {
-              Header: 'Entities',
-              columns: [
-                {
-                  Header: 'Company Name',
-                  accessor: 'name'
-                },
-                {
-                  Header: 'Industry Type',
-                  id: 'industry',
-                  accessor: 'industry'
-                }
-              ]
-            }
-          ]}
+          columns={columns}
+          // filtered={[
+          //   {
+          //     id: 'industry',
+          //     value: 'Basic Materials'
+          //   } 
+          // ]}
           defaultPageSize={10}
           className="-striped -highlight"
           defaultSorted={[
